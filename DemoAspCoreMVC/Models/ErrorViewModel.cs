e161f92689182faf1d614a5fbdbd918dcb615a93
KOUSHIK SAHA <koushik.official1999@gmail.com>
2020-04-03 11:45:05 +0530

    Add project files.

---
+++ DemoAspCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoAspCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
